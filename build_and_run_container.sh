684e31bbeb8ff37bf202be3e5fbebd01925d78b0
KrArunT <145754819+KrArunT@users.noreply.github.com>
2025-05-19 18:39:58 +0530

    Create build_and_run_container.sh

---
+++ build_and_run_container.sh
@@ -0,0 +1,8 @@
+docker build -t va_demo_ws_server:latest .
+docker run --rm \
+  -it \
+  -p 8768:8768 \
+  -p 15672:15672 \
+  -p 22:22 \
+  -e ANSIBLE_HOST_KEY_CHECKING=False \
+  va_demo_ws_server:latest
