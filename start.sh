0fcc4a604a99ad65c91c977deb4e7d8b66b34457
KrArunT <145754819+KrArunT@users.noreply.github.com>
2025-05-22 10:31:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-# #!/bin/bash
+#!/bin/bash
 # export ANSIBLE_HOST_KEY_CHECKING=False
 # uv run ansible-playbook -i inventory.yml run_scripts.yml
 
-#!/bin/bash
 scp start_va.sh va_amd:/home/amd/workspace/vademo
 ssh va_amd  "/home/amd/workspace/vademo/start_va.sh"
