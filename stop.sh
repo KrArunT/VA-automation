895980b41ba11929cadddbbcc9998a99deda9ab4
KrArunT <145754819+KrArunT@users.noreply.github.com>
2025-05-22 10:31:00 +0530

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -2,6 +2,5 @@
 # export ANSIBLE_HOST_KEY_CHECKING=False
 # uv run ansible-playbook -i inventory.yml kill_scripts.yml
 
-#!/bin/bash
 scp stop_va.sh va_amd:/home/amd/workspace/vademo
 ssh va_amd  "/home/amd/workspace/vademo/stop_va.sh"
